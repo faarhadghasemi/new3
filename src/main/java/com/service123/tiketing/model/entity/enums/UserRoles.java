499aa00481b4b6ebf204b5610fb07a74b3fe52b6
faarhadghasemi <farhadghasemi.official@gmail.com>
2023-09-21 16:41:35 +0330

    continue

---
+++ src/main/java/com/service123/tiketing/model/entity/enums/UserRoles.java
@@ -0,0 +1,7 @@
+package com.service123.tiketing.model.entity.enums;
+
+public enum UserRoles {
+    admin,
+    customer,
+    employee
+}
--- src/main/java/com/service123/tiketing/model/entity/enums/person1.java
+++
@@ -1,4 +0,0 @@
-package com.service123.tiketing.model.entity.enums;
-
-public enum person1 {
-}
