fa1aacecae836b0557be8e6b5d5d496ceb79dd05
arman <mrezabkh97@gmail.com>
2023-09-22 14:22:27 +0330

    peoblem entity and repository update

--- src/main/java/com/service123/tiketing/model/entity/Problem.java
+++ src/main/java/com/service123/tiketing/model/entity/Problem.java
@@ -1,4 +1,33 @@
 package com.service123.tiketing.model.entity;
 
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+
 public class Problem {
+    private long id;
+    private String describtion;
+    private LocalDate datetime;
+    private boolean deleted;
+
+
+
+
+
+    @Override
+    public String toString() {
+
+        return new Gson().toJson(this);
+    }
+
+
 }
--- src/main/java/com/service123/tiketing/model/repository/ProblemRepository.java
+++ src/main/java/com/service123/tiketing/model/repository/ProblemRepository.java
@@ -1,4 +1,89 @@
 package com.service123.tiketing.model.repository;
 
-public class ProblemRepository {
+import com.service123.tiketing.controller.exception.ContentNotFoundException;
+import com.service123.tiketing.model.common.Jdbc;
+import com.service123.tiketing.model.entity.Problem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+public class ProblemRepository implements RepositoryImpl<Problem> {
+
+    private Connection connection;
+    private PreparedStatement statement;
+
+    public ProblemRepository() {
+    }
+
+    @Override
+    public Problem save(Problem problem) throws Exception {
+
+        connection = Jdbc.getJdbc().getConnection();
+        statement = connection.prepareStatement(
+                "INSERT INTO PROBLEM_TBL values id=?,describtion=?,deleted=?"
+        );
+        statement.setLong(1, problem.getId());
+        statement.setString(2, problem.getDescribtion());
+        statement.setBoolean(3, problem.isDeleted());
+        statement.execute();
+
+        return problem;
+    }
+
+    @Override
+    public Problem edit(Problem problem) throws Exception {
+
+        connection = Jdbc.getJdbc().getConnection();
+        statement = connection.prepareStatement(
+                "Select * FROM PROBLEM_TBL where id=? "
+        );
+        statement.setLong(1, problem.getId());
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            connection = Jdbc.getJdbc().getConnection();
+            statement = connection.prepareStatement(
+                    "Update PROBLEM_TBL set id=?,describtion=? and deleted=0"
+            );
+            statement.setLong(1, problem.getId());
+            statement.setString(2, problem.getDescribtion());
+        } else {
+            problem = null;
+            throw new ContentNotFoundException("problem  Not Found");
+
+        }
+        return problem;
+    }
+
+    @Override
+    public Problem remove(long id) throws Exception {
+        Problem problem=null;
+        connection = Jdbc.getJdbc().getConnection();
+        statement = connection.prepareStatement(
+                "SELECT * FROM PROBLEM_TBL WHERE id=?"
+        );
+//        statement.setLong(1,problem.getId());
+        ResultSet resultSet = statement.executeQuery();
+
+
+
+        return null;
+    }
+
+    @Override
+    public List<Problem> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Problem findById(long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        connection.close();
+        statement.close();
+    }
 }
