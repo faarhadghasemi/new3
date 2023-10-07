827f2d6f1d3db17d707c6f3cd0c1943565b143f8
bakhtiarian.m <mrezabkh97@gmail.com>
2023-10-07 16:52:49 +0330

    ProblemRepository complete since findall

--- src/main/java/com/service123/tiketing/model/repository/ProblemRepository.java
+++ src/main/java/com/service123/tiketing/model/repository/ProblemRepository.java
@@ -3,7 +3,10 @@ package com.service123.tiketing.model.repository;
 import com.service123.tiketing.controller.exception.ContentNotFoundException;
 import com.service123.tiketing.model.common.Jdbc;
 import com.service123.tiketing.model.entity.Problem;
+import com.service123.tiketing.model.entity.User;
+
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -22,11 +25,15 @@ public class ProblemRepository implements RepositoryImpl<Problem> {
 
         connection = Jdbc.getJdbc().getConnection();
         statement = connection.prepareStatement(
-                "INSERT INTO PROBLEM_TBL values id=?,describtion=?,deleted=?"
+                "INSERT INTO PROBLEM_TBL values id=?,describtion=?,dateTime=?,sender=?,receiver=?,answer=?,deleted=? and status=Pending and ,parentId=1"
         );
         statement.setLong(1, problem.getId());
         statement.setString(2, problem.getDescription());
-        statement.setBoolean(3, problem.isDeleted());
+        statement.setDate(3, Date.valueOf(problem.getDateTime()));
+        statement.setString(4, String.valueOf(problem.getSender()));
+        statement.setString(5, String.valueOf(problem.getReceiver()));
+        statement.setString(6, problem.getAnswer());
+        statement.setBoolean(7, problem.isDeleted());
         statement.execute();
 
         return problem;
@@ -44,10 +51,16 @@ public class ProblemRepository implements RepositoryImpl<Problem> {
         if (resultSet.next()) {
             connection = Jdbc.getJdbc().getConnection();
             statement = connection.prepareStatement(
-                    "Update PROBLEM_TBL set id=?,describtion=? and deleted=0"
+                    "Update PROBLEM_TBL set id=?,describtion=?,dateTime=?,sender=?,receiver=?,answer=?,describtion=? and deleted=0 and status=Pending and parenti=id "
             );
             statement.setLong(1, problem.getId());
             statement.setString(2, problem.getDescription());
+            statement.setDate(3, Date.valueOf(problem.getDateTime()));
+            statement.setString(4, String.valueOf(problem.getSender()));
+            statement.setString(5, String.valueOf(problem.getReceiver()));
+            statement.setString(6, problem.getAnswer());
+            statement.setBoolean(7, problem.isDeleted());
+            statement.execute();
         } else {
             problem = null;
             throw new ContentNotFoundException("problem  Not Found");
@@ -61,18 +74,21 @@ public class ProblemRepository implements RepositoryImpl<Problem> {
         Problem problem=null;
         connection = Jdbc.getJdbc().getConnection();
         statement = connection.prepareStatement(
-                "SELECT * FROM PROBLEM_TBL WHERE id=?"
+               "SELECT * FROM PROBLEM_TBL WHERE id=?"
         );
-        statement.setLong(1,problem.getId());
+        statement.setLong(1,id);
         ResultSet resultSet = statement.executeQuery();
-        Problem problem1=null;
         if (resultSet.next()){
             Problem.builder()
                     .id(resultSet.getLong("id"))
                     .description(resultSet.getString("description"))
+                    .dateTime(resultSet.getDate("datetime").toLocalDate())
+//                    .sender(resultSet.getString("sender"))
+//                    .receiver(resultSet.getString("sender"))
+                    .answer(resultSet.getString("answer"))
                     .build();
         }
-        if(problem != null){
+        if (problem != null){
             statement = connection.prepareStatement(
                     "Update  PROBLEM_TBL set deleted=1 where id=?");
             statement.setLong(1,problem.getId());
@@ -128,10 +144,6 @@ public class ProblemRepository implements RepositoryImpl<Problem> {
             throw new ContentNotFoundException("problem not found");
         }
         return problem;
-    }
-
-    public Problem login(Problem problem) throws Exception {
-        return null;
     }
 
     @Override
