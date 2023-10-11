b0f1e339f902d82473f0340faeead7a01bfc703d
faarhadghasemi <farhadghasemi.official@gmail.com>
2023-10-11 19:02:15 +0330

    teacher do commit

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -1,10 +1,18 @@
-let container = document.getElementById('container')
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+    signupBtn.click();
+    return false;
+});
 
-toggle = () => {
-    container.classList.toggle('sign-in')
-    container.classList.toggle('sign-up')
-}
-
-setTimeout(() => {
-    container.classList.add('sign-in')
-}, 200)
