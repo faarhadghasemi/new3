82feab830fd291725178b3992fce47e825abff31
mamrez <mrezabkh97@gmail.com>
2023-11-12 01:18:09 +0330

    login error and invalid username updated

--- src/main/java/com/service123/tiketing/model/repository/UserRepository.java
+++ src/main/java/com/service123/tiketing/model/repository/UserRepository.java
@@ -180,8 +180,8 @@ public class UserRepository implements RepositoryImpl<User> {
         statement.setString(1, username);
         statement.setString(2, password);
         ResultSet resultSet = statement.executeQuery();
-        resultSet.next();
-        return (username != null);
+        boolean next = resultSet.next();
+        return (next);
     }
 //-------------------------------------------------------------------------
 //    public boolean findByUserName(String username) throws Exception {
