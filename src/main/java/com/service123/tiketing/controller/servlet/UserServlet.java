7471660488d42bfe556ee38ee5380f9e5235a282
mamrez <mrezabkh97@gmail.com>
2023-11-14 22:35:34 +0330

    UserServlet Updated

--- src/main/java/com/service123/tiketing/controller/servlet/UserServlet.java
+++ src/main/java/com/service123/tiketing/controller/servlet/UserServlet.java
@@ -35,10 +35,10 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println((request.getParameter("name")));
-        System.out.println((request.getParameter("family")));
-        System.out.println((request.getParameter("username")));
-        System.out.println((request.getParameter("password")));
+//        System.out.println((request.getParameter("name")));
+//        System.out.println((request.getParameter("family")));
+//        System.out.println((request.getParameter("username")));
+//        System.out.println((request.getParameter("password")));
 //    }
 //}
 
